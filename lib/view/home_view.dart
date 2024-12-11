a75fd8d1087b7c5f4e30f3e54b8724d73217ce21
nguyen dao <nguyendao21012002@gmail.com>
2024-12-11 08:49:14 +0700

    complete home

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -32,7 +32,6 @@ class _HomeViewState extends State<HomeView> {
   bool _isLoadingStores = false;
   bool _isPlacingOrder = false;
 
-
   @override
   void initState() {
     super.initState();
@@ -47,6 +46,7 @@ class _HomeViewState extends State<HomeView> {
       _isLoadingProducts = false;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,9 +75,7 @@ class _HomeViewState extends State<HomeView> {
                       ),
                       const SizedBox(height: 8),
                       Text(
-                        controller.userData['address']
-                            .toString()
-                            .toUpperCase(),
+                        controller.userData['address'].toString().toUpperCase(),
                         style: const TextStyle(
                           fontSize: 14,
                           color: Colors.black,
@@ -127,7 +125,7 @@ class _HomeViewState extends State<HomeView> {
                   children: [
                     TextSpan(
                       text:
-                      'Hey ${controller.userData['fullName']?.toString() ?? 'User'} ,',
+                          'Hey ${controller.userData['fullName']?.toString() ?? 'User'} ,',
                     ),
                     const TextSpan(
                       text: ' Have a good Day!',
@@ -146,10 +144,14 @@ class _HomeViewState extends State<HomeView> {
                   controller: controller.searchController,
                   decoration: InputDecoration(
                     hintText: 'Search dishes, restaurants',
-                    hintStyle: const TextStyle(color: Color(0xff32343E), fontFamily: 'Poppins', fontWeight: FontWeight.w400, fontSize: 14),
+                    hintStyle: const TextStyle(
+                        color: Color(0xff32343E),
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14),
                     prefixIcon: SizedBox(
                       height: 16, // Chiều cao mới
-                      width: 16,  // Chiều rộng mới
+                      width: 16, // Chiều rộng mới
                       child: Image.asset(
                         ImageAsset.searchHome,
                       ),
@@ -157,16 +159,20 @@ class _HomeViewState extends State<HomeView> {
                     suffixIcon: IconButton(
                       icon: Image.asset(
                         controller.isListening
-                            ? ImageAsset.microphone  // Hiển thị hình ảnh micro khi đang nghe
-                            : ImageAsset.microphoneUn,  // Hiển thị hình ảnh micro khi không nghe
-                        height: 24,  // Đặt chiều cao của hình ảnh (tuỳ chỉnh theo nhu cầu)
-                        width: 24,   // Đặt chiều rộng của hình ảnh (tuỳ chỉnh theo nhu cầu)
+                            ? ImageAsset
+                                .microphone // Hiển thị hình ảnh micro khi đang nghe
+                            : ImageAsset
+                                .microphoneUn, // Hiển thị hình ảnh micro khi không nghe
+                        height:
+                            24, // Đặt chiều cao của hình ảnh (tuỳ chỉnh theo nhu cầu)
+                        width:
+                            24, // Đặt chiều rộng của hình ảnh (tuỳ chỉnh theo nhu cầu)
                       ),
                       onPressed: () {
-                        controller.toggleListening();  // Chuyển đổi trạng thái của micro
+                        controller
+                            .toggleListening(); // Chuyển đổi trạng thái của micro
                       },
                     ),
-
                     filled: true,
                     fillColor: const Color(0xffB2EBF2),
                     contentPadding: const EdgeInsets.symmetric(
@@ -183,65 +189,95 @@ class _HomeViewState extends State<HomeView> {
                   },
                 ),
               ),
-
-              const SizedBox(height: 20,),
+              const SizedBox(
+                height: 20,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const SizedBox(width: 4,),
+                  const SizedBox(
+                    width: 4,
+                  ),
                   GestureDetector(
-                    onTap: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ComboView(listDS: controllerTestView.products,),
-                        ),
-                      );
-                    },
-                      child: HomeInWell(image: ImageAsset.combo, text: 'Combo',)),
-                  GestureDetector(
-                      onTap: (){
+                      onTap: () {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => ChickenView(),
+                            builder: (context) => ComboView(
+                              listDS: controllerTestView.products,
+                            ),
                           ),
                         );
                       },
-                      child: HomeInWell(image: ImageAsset.chicken, text: 'Chicken',)),
+                      child: HomeInWell(
+                        image: ImageAsset.combo,
+                        text: 'Combo',
+                      )),
                   GestureDetector(
-                    onTap: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => BurgerView(listDS: controllerTestView.products,),
-                        ),
-                      );
-                    },
-                      child: HomeInWell(image: ImageAsset.burger, text: 'Burger',)),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChickenView(
+                              listDS: controllerTestView.products,
+                            ),
+                          ),
+                        );
+                      },
+                      child: HomeInWell(
+                        image: ImageAsset.chicken,
+                        text: 'Chicken',
+                      )),
                   GestureDetector(
-                    onTap: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SnacksView(),
-                        ),
-                      );
-                    },
-                      child: HomeInWell(image: ImageAsset.snack, text: 'Snacks',)),
-                  const SizedBox(width: 4,),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BurgerView(
+                              listDS: controllerTestView.products,
+                            ),
+                          ),
+                        );
+                      },
+                      child: HomeInWell(
+                        image: ImageAsset.burger,
+                        text: 'Burger',
+                      )),
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SnacksView(
+                              listDS: controllerTestView.products,
+                            ),
+                          ),
+                        );
+                      },
+                      child: HomeInWell(
+                        image: ImageAsset.snack,
+                        text: 'Snacks',
+                      )),
+                  const SizedBox(
+                    width: 4,
+                  ),
                 ],
               ),
-              const SizedBox(height: 20,),
+              const SizedBox(
+                height: 20,
+              ),
               Container(
                 height: 211,
                 width: double.infinity,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20), // Chỉnh góc bo tròn
-                  border: Border.all(color: Colors.white, width: 2), // Đường viền của container
+                  border: Border.all(
+                      color: Colors.white,
+                      width: 2), // Đường viền của container
                 ),
                 child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20), // Đảm bảo ảnh cũng có góc bo tròn như container
+                  borderRadius: BorderRadius.circular(
+                      20), // Đảm bảo ảnh cũng có góc bo tròn như container
                   child: Image.asset(
                     ImageAsset.homeChicken,
                     fit: BoxFit.cover, // Làm ảnh fill toàn bộ container
@@ -249,11 +285,13 @@ class _HomeViewState extends State<HomeView> {
                 ),
               ),
               InkWell(
-                  onTap: (){
+                  onTap: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => ComboView(listDS: controllerTestView.products,),
+                        builder: (context) => ComboView(
+                          listDS: controllerTestView.products,
+                        ),
                       ),
                     );
                   },
@@ -263,116 +301,117 @@ class _HomeViewState extends State<HomeView> {
                 child: _isLoadingProducts
                     ? const Center(child: CircularProgressIndicator())
                     : ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: _products.length,
-                  itemBuilder: (context, index) {
-                    final product = _products[index];
-                    if (product['Category'] == 'Combo 1 Người' || product['Category'] == 'Combo Nhóm') {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ProductCard(product: product),
-                      );
-                    }
-                    return const SizedBox.shrink(); // Ẩn các sản phẩm không phù hợp
-                  },
-                ),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _products.length,
+                        itemBuilder: (context, index) {
+                          final product = _products[index];
+                          if (product['Category'] == 'Combo 1 Người' ||
+                              product['Category'] == 'Combo Nhóm') {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: ProductCard(product: product),
+                            );
+                          }
+                          return const SizedBox
+                              .shrink(); // Ẩn các sản phẩm không phù hợp
+                        },
+                      ),
               ),
-
               InkWell(
-                  onTap: (){
-
-                  },
-                  child: const TitleSeeMore(title: 'Chicken')),
+                  onTap: () {}, child: const TitleSeeMore(title: 'Chicken')),
               SizedBox(
                 height: 285, // Chiều cao cố định cho danh sách ngang
                 child: _isLoadingProducts
                     ? const Center(child: CircularProgressIndicator())
                     : ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: _products.length,
-                  itemBuilder: (context, index) {
-                    final product = _products[index];
-                    if (product['Category'] == 'Gà Rán - Gà Quay') {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ProductCard(product: product),
-                      );
-                    }
-                    return const SizedBox.shrink(); // Ẩn các sản phẩm không phù hợp
-                  },
-                ),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _products.length,
+                        itemBuilder: (context, index) {
+                          final product = _products[index];
+                          if (product['Category'] == 'Gà Rán - Gà Quay') {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: ProductCard(product: product),
+                            );
+                          }
+                          return const SizedBox
+                              .shrink(); // Ẩn các sản phẩm không phù hợp
+                        },
+                      ),
               ),
               InkWell(
-                  onTap: (){
-
-                  },
-                  child: const TitleSeeMore(title: 'Burger - Rice - Spaghetti')),
+                  onTap: () {},
+                  child:
+                      const TitleSeeMore(title: 'Burger - Rice - Spaghetti')),
               SizedBox(
                 height: 285, // Chiều cao cố định cho danh sách ngang
                 child: _isLoadingProducts
                     ? const Center(child: CircularProgressIndicator())
                     : ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: _products.length,
-                  itemBuilder: (context, index) {
-                    final product = _products[index];
-                    if (product['Category'] == 'Burger - Cơm - Mì Ý') {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ProductCard(product: product),
-                      );
-                    }
-                    return const SizedBox.shrink(); // Ẩn các sản phẩm không phù hợp
-                  },
-                ),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _products.length,
+                        itemBuilder: (context, index) {
+                          final product = _products[index];
+                          if (product['Category'] == 'Burger - Cơm - Mì Ý') {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: ProductCard(product: product),
+                            );
+                          }
+                          return const SizedBox
+                              .shrink(); // Ẩn các sản phẩm không phù hợp
+                        },
+                      ),
               ),
-              InkWell(
-                  onTap: (){
-
-                  },
-                  child: const TitleSeeMore(title: 'Snacks')),
+              InkWell(onTap: () {}, child: const TitleSeeMore(title: 'Snacks')),
               SizedBox(
                 height: 285, // Chiều cao cố định cho danh sách ngang
                 child: _isLoadingProducts
                     ? const Center(child: CircularProgressIndicator())
                     : ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: _products.length,
-                  itemBuilder: (context, index) {
-                    final product = _products[index];
-                    if (product['Category'] == 'Thức ăn nhẹ') {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ProductCard(product: product),
-                      );
-                    }
-                    return const SizedBox.shrink(); // Ẩn các sản phẩm không phù hợp
-                  },
-                ),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _products.length,
+                        itemBuilder: (context, index) {
+                          final product = _products[index];
+                          if (product['Category'] == 'Thức ăn nhẹ') {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: ProductCard(product: product),
+                            );
+                          }
+                          return const SizedBox
+                              .shrink(); // Ẩn các sản phẩm không phù hợp
+                        },
+                      ),
               ),
               InkWell(
-                  onTap: (){
-
-                  }
-                  ,child: const TitleSeeMore(title: 'Drinks And Desserts')),
+                  onTap: () {},
+                  child: const TitleSeeMore(title: 'Drinks And Desserts')),
               SizedBox(
                 height: 285, // Chiều cao cố định cho danh sách ngang
                 child: _isLoadingProducts
                     ? const Center(child: CircularProgressIndicator())
                     : ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: _products.length,
-                  itemBuilder: (context, index) {
-                    final product = _products[index];
-                    if (product['Category'] == 'Thức uống & tráng miệng') {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ProductCard(product: product),
-                      );
-                    }
-                    return const SizedBox.shrink(); // Ẩn các sản phẩm không phù hợp
-                  },
-                ),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _products.length,
+                        itemBuilder: (context, index) {
+                          final product = _products[index];
+                          if (product['Category'] ==
+                              'Thức uống & tráng miệng') {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: ProductCard(product: product),
+                            );
+                          }
+                          return const SizedBox
+                              .shrink(); // Ẩn các sản phẩm không phù hợp
+                        },
+                      ),
               ),
               const SizedBox(height: 20),
             ],
