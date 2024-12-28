fe162eeace67a7e9d482145faf21818b18d747c4
nguyen dao <nguyendao21012002@gmail.com>
2024-12-28 17:58:16 +0700

    fix bug order tracking

--- lib/widgets/common_widget/order_tracking/waiting_for_delivery.dart
+++ lib/widgets/common_widget/order_tracking/waiting_for_delivery.dart
@@ -22,15 +22,36 @@ class _WaitingForDeliveryState extends State<WaitingForDelivery> {
   @override
   void initState() {
     super.initState();
+    getDataViewModel.fetchOrderTracking();
+    getDataViewModel.orderTracking;
     _loadOrderTracking();
   }
 
   Future<void> _loadOrderTracking() async {
-    await getDataViewModel.fetchProducts(); // Fetch products or orders data
-    setState(() {
-      _orderTracking = getDataViewModel.orderTracking;
-      _isLoadingOrderTracking = false;
-    });
+    try {
+      setState(() {
+        _isLoadingOrderTracking = true;
+      });
+      // Fetch data từ server
+      await getDataViewModel.fetchProducts();
+      // Kiểm tra log dữ liệu mới
+      print("Fetched orderTracking: ${getDataViewModel.orderTracking}");
+
+      setState(() {
+        _orderTracking = getDataViewModel.orderTracking;
+      });
+    } catch (e) {
+      print("Error loading data: $e");
+      Get.snackbar(
+        "Error",
+        "Failed to load data. Please try again later.",
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    } finally {
+      setState(() {
+        _isLoadingOrderTracking = false;
+      });
+    }
   }
 
   @override
@@ -38,146 +59,143 @@ class _WaitingForDeliveryState extends State<WaitingForDelivery> {
     return Scaffold(
       body: _isLoadingOrderTracking
           ? const Center(child: CircularProgressIndicator())
-          : (_orderTracking.isEmpty
-          ? const Center(child: Text("Your Confirm Cart is empty."))
-          : SingleChildScrollView(
-        child: Column(
-          children: [
-            // Loop through the order tracking data and display
-            for (var order in _orderTracking) ...[
-              // Wrap the order details and products inside a Container
-              if((order['userId'].toString() == ordersViewModel.userId.toString()) && (order['status'].toString() == 'Waiting Delivery'))
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8.0,
-                    horizontal: 12.0,
-                  ),
-                  padding: const EdgeInsets.all(12.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 3,
-                        blurRadius: 7,
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // Display Order ID
-                      Text(
-                        'Order ID: ${order['orderId']}',
-                        style: const TextStyle(
-                          fontSize: 18,
-                          color: Color(0xff1C1B1F),
-                          fontWeight: FontWeight.w600,
-                          fontFamily: 'Poppins',
+          : RefreshIndicator(
+        onRefresh: _loadOrderTracking,
+        child: _orderTracking.isEmpty
+            ? const Center(child: Text("Your Confirm Cart is empty."))
+            : SingleChildScrollView(
+          child: Column(
+            children: [
+              for (var order in _orderTracking) ...[
+                if((order['userId'].toString() == ordersViewModel.userId.toString()) && (order['status'].toString() == 'Waiting Delivery'))
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 12.0,
+                    ),
+                    padding: const EdgeInsets.all(12.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 3,
+                          blurRadius: 7,
                         ),
-                      ),
-                      // Loop through the list of products and display each product
-                      if (order['listProducts'] is List)
-                        for (var product in order['listProducts'])
-                          Card(
-                            margin: const EdgeInsets.symmetric(
-                              vertical: 8.0,
-                              horizontal: 12,
-                            ),
-                            child: ListTile(
-                              leading: ClipRRect(
-                                borderRadius: BorderRadius.circular(8),
-                                child: Image.network(
-                                  product['imageURL'] ?? '',
-                                  width: 80,
-                                  height: 80,
-                                  fit: BoxFit.cover,
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Order ID: ${order['orderId']}',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Color(0xff1C1B1F),
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Poppins',
+                          ),
+                        ),
+                        if (order['listProducts'] is List)
+                          for (var product
+                          in order['listProducts'])
+                            Card(
+                              margin: const EdgeInsets.symmetric(
+                                vertical: 8.0,
+                                horizontal: 12,
+                              ),
+                              child: ListTile(
+                                leading: ClipRRect(
+                                  borderRadius:
+                                  BorderRadius.circular(8),
+                                  child: Image.network(
+                                    product['imageURL'] ?? '',
+                                    width: 80,
+                                    height: 80,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                title: Text(
+                                  product['nameProduct'] ?? '',
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xff1C1B1F),
+                                    fontWeight: FontWeight.w600,
+                                    fontFamily: 'Poppins',
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  "Quantity: ${product['quantities']}",
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    color: Color(0xff1C1B1F),
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: 'Poppins',
+                                  ),
                                 ),
                               ),
-                              title: Text(
-                                product['nameProduct'] ?? '',
+                            ),
+                        const SizedBox(height: 4),
+                        const Divider(),
+                        const SizedBox(height: 4),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16),
+                          child: Column(
+                            crossAxisAlignment:
+                            CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Address: ${order['deliveryAddress']}',
                                 style: const TextStyle(
                                   fontSize: 16,
                                   color: Color(0xff1C1B1F),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: 'Poppins',
+                                ),
+                              ),
+                              const SizedBox(height: 4.0),
+                              Text(
+                                'Payment Method: ${order['paymentMethod']}',
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Color(0xff1C1B1F),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: 'Poppins',
+                                ),
+                              ),
+                              const SizedBox(height: 4.0),
+                              Text(
+                                'Place of Purchase: ${order['placeOfPurchase']}',
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Color(0xff1C1B1F),
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: 'Poppins',
+                                ),
+                              ),
+                              const SizedBox(height: 4.0),
+                              Text(
+                                'Total: ${order['total']} VND',
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Color(0xffA02334),
                                   fontWeight: FontWeight.w600,
                                   fontFamily: 'Poppins',
                                 ),
                               ),
-                              subtitle: Text(
-                                "Quantity: ${product['quantities']}",
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  color: Color(0xff1C1B1F),
-                                  fontWeight: FontWeight.w400,
-                                  fontFamily: 'Poppins',
-                                ),
-                              ),
-                            ),
+                            ],
                           ),
-                      const SizedBox(height: 4,),
-                      const Divider(),
-                      const SizedBox(height: 4,),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Address: ${order['deliveryAddress']}',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: Color(0xff1C1B1F),
-                                fontWeight: FontWeight.w500,
-                                fontFamily: 'Poppins',
-                              ),
-                            ),
-                            const SizedBox(height: 4.0),
-                            // Display Payment Method
-                            Text(
-                              'Payment Method: ${order['paymentMethod']}',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: Color(0xff1C1B1F),
-                                fontWeight: FontWeight.w500,
-                                fontFamily: 'Poppins',
-                              ),
-                            ),
-                            const SizedBox(height: 4.0),
-                            // Display Place of Purchase
-                            Text(
-                              'Place of Purchase: ${order['placeOfPurchase']}',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: Color(0xff1C1B1F),
-                                fontWeight: FontWeight.w500,
-                                fontFamily: 'Poppins',
-                              ),
-                            ),
-                            const SizedBox(height: 4.0),
-                            // Display Purchase Date
-                            const SizedBox(height: 4.0),
-                            // Display Total
-                            Text(
-                              'Total: ${order['total']} VND',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: Color(0xffA02334),
-                                fontWeight: FontWeight.w600,
-                                fontFamily: 'Poppins',
-                              ),
-                            ),
-                          ],
                         ),
-                      ),
-
-                    ],
+                      ],
+                    ),
                   ),
-                ),
+              ],
             ],
-          ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
